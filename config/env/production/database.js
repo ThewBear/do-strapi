a3c19bc084aae0c9ca031b4d0544bb56b0d07b39
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-01-03 22:17:21 +0700

    Add DATABASE_CA env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,9 @@ module.exports = ({ env }) => ({
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: {
+          ca: env("DATABASE_CA", ""),
+        },
       },
     },
   },
