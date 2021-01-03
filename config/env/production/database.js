5be01794b5f7aa1590d2d02dd5f1c8319064f728
ThewBear <26767607+ThewBear@users.noreply.github.com>
2021-01-03 21:49:39 +0700

    Add DATABASE_SSL env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         database: env("DATABASE_NAME", "strapi"),
         username: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: true,
+        ssl: env.bool('DATABASE_SSL', false),
       },
     },
   },
